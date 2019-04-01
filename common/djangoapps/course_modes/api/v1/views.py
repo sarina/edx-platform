f2fc5f55879c47d8f59ccbc79707fc078bb22540
Alex Dusenbery <adusenbery@edx.org>
2019-04-01 16:46:21 -0400

    WIP: docstrings [skip ci]

--- common/djangoapps/course_modes/api/v1/views.py
+++ common/djangoapps/course_modes/api/v1/views.py
@@ -39,7 +39,47 @@ class CourseModesMixin(object):
 
 class CourseModesView(CourseModesMixin, ListCreateAPIView):
     """
-    View to list and create course modes for a course.
+    View to list or create course modes for a course.
+
+    **Use Case**
+
+        List all course modes for a course, or create a new
+        course mode.
+
+    **Example Requests**
+
+        GET /api/course_modes/v1/courses/{course_id}/
+
+            Returns a list of all existing course modes for a course.
+
+        POST /api/course_modes/v1/courses/{course_id}/
+
+            Creates a new course mode in a course.
+
+    **Response Values**
+
+        For each HTTP verb below, an HTTP 404 "Not Found" response is returned if the
+        requested course id does not exist.
+
+        GET: If the request is successful, an HTTP 200 "OK" response is returned
+        along with a list of course mode dictionaries within a course.
+        The details are contained in a JSON dictionary as follows:
+
+          * course_id: The course identifier.
+          * mode_slug: The short name for the course mode.
+          * mode_display_name: The verbose name for the course mode.
+          * min_price: The minimum price for which a user can
+            enroll in this mode.
+          * currency: The currency of the listed prices.
+          * expiration_datetime: The date and time after which
+            users cannot enroll in the course in this mode (not required for POST).
+          * expiration_datetime_is_explicit: Whether the expiration_datetime field was
+            explicitly set (not required for POST).
+          * description: A description of this mode (not required for POST).
+          * sku: The SKU for this mode (for ecommerce purposes, not required for POST).
+          * bulk_sku: The bulk SKU for this mode (for ecommerce purposes, not required for POST).
+
+        POST: If the request is successful, an HTTP 201 "Created" response is returned.
     """
     pass
 
@@ -48,7 +88,53 @@ class CourseModesDetailView(CourseModesMixin, RetrieveUpdateDestroyAPIView):
     """
     View to retrieve, update, or delete a specific course mode for a course.
 
-    If "application/merge-patch+json" is not the specified content type, a 415 "Unsupported Media Type" response is returned.
+    **Use Case**
+
+        Get or update course mode details for a specific course mode on a course.
+        Or you may delete a specific course mode from a course.
+
+    **Example Requests**
+
+        GET /api/course_modes/v1/courses/{course_id}/{mode_slug}
+
+            Returns details on an existing course mode for a course.
+
+        PATCH /api/course_modes/v1/courses/{course_id}/{mode_slug}
+
+            Updates (via merge) details of an existing course mode for a course.
+
+        DELETE /api/course_modes/v1/courses/{course_id}/{mode_slug}
+
+            Deletes an existing course mode for a course.
+
+    **Response Values**
+
+        For each HTTP verb below, an HTTP 404 "Not Found" response is returned if the
+        requested course id does not exist, or the mode slug does not exist within the course.
+
+        GET: If the request is successful, an HTTP 200 "OK" response is returned
+        along with a details for a single course mode within a course.  The details are contained
+        in a JSON dictionary as follows:
+
+          * course_id: The course identifier.
+          * mode_slug: The short name for the course mode.
+          * mode_display_name: The verbose name for the course mode.
+          * min_price: The minimum price for which a user can
+            enroll in this mode.
+          * currency: The currency of the listed prices.
+          * expiration_datetime: The date and time after which
+            users cannot enroll in the course in this mode (not required for PATCH).
+          * expiration_datetime_is_explicit: Whether the expiration_datetime field was
+            explicitly set (not required for PATCH).
+          * description: A description of this mode (not required for PATCH).
+          * sku: The SKU for this mode (for ecommerce purposes, not required for PATCH).
+          * bulk_sku: The bulk SKU for this mode (for ecommerce purposes, not required for PATCH).
+
+        PATCH: If the request is successful, an HTTP 204 "No Content" response is returned.
+        If "application/merge-patch+json" is not the specified content type,
+        a 415 "Unsupported Media Type" response is returned.
+
+        DELETE: If the request is successful, an HTTP 204 "No Content" response is returned.
     """
     http_method_names = ['get', 'patch', 'delete', 'head', 'options']
     parser_classes = (MergePatchParser,)
